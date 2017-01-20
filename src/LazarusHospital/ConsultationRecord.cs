afcdecc3687c2166cd34e2e3c4cc67320cd4d70b
Adam Furtenbach <adam.furtenbach@activesolution.se>
2017-01-20 21:42:20 +0100

    ConsultationRecord

--- src/LazarusHospital/ConsultationRecord.cs
+++ src/LazarusHospital/ConsultationRecord.cs
@@ -6,9 +6,19 @@ namespace LazarusHospital
 {
     public class ConsultationRecord
     {
+        public Patient Patient { get; }
+        public Doctor Doctor { get; }
+        public TreatmentRoom TreatmentRoom { get; }
+        public DateTime RegistrationDate { get; }
+        public DateTime ConsolutationDate { get; }
+
         public ConsultationRecord(Patient patient, Doctor doctor, TreatmentRoom treatmentRoom, DateTime registrationDate, DateTime consolutationDate)
         {
-            
+            Patient = patient;
+            Doctor = doctor;
+            TreatmentRoom = treatmentRoom;
+            RegistrationDate = registrationDate;
+            ConsolutationDate = consolutationDate;
         }
     }
 }
