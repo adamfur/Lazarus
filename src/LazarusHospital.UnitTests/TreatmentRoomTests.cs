0c53b26b050d43c43e4ced09ca72b4fc2ffc5ab5
Adam Furtenbach <adam.furtenbach@gmail.com>
2017-01-20 21:06:55 +0100

    .

--- src/LazarusHospital.UnitTests/TreatmentRoomTests.cs
+++ src/LazarusHospital.UnitTests/TreatmentRoomTests.cs
@@ -19,8 +19,8 @@ namespace LazarusHospital.UnitTests
         public TreatmentRoomTests()
         {
             _patientWithCancerHead = new Patient("CancerHead", new Cancer(Topology.Head));
-            _patientWithCancerNeck = new Patient("CancerHead", new Cancer(Topology.Neck));
-            _patientWithCancerBreast = new Patient("CancerHead", new Cancer(Topology.Breast));
+            _patientWithCancerNeck = new Patient("CancerNeck", new Cancer(Topology.Neck));
+            _patientWithCancerBreast = new Patient("CancerBreast", new Cancer(Topology.Breast));
             _patientWithFlu = new Patient("Flu", new Flu());
 
             _advancedTreatmentRoom = new TreatmentRoom("Advanced", new AdvancedTreatmentMachine("AdvancedMachinery"));
@@ -31,25 +31,97 @@ namespace LazarusHospital.UnitTests
         [Fact]
         public void Advanced_Treatment_Room_can_treat_patient_with_head_cancer()
         {
-            
+            var result = _patientWithCancerHead.CanBeTreatedBy(_advancedTreatmentRoom);
+
+            Assert.True(result);
         }
 
         [Fact]
         public void Advanced_Treatment_Room_can_treat_patient_with_neck_cancer()
         {
+            var result = _patientWithCancerNeck.CanBeTreatedBy(_advancedTreatmentRoom);
 
+            Assert.True(result);
         }
 
         [Fact]
         public void Advanced_Treatment_Room_can_not_treat_patient_with_breast_cancer()
         {
+            var result = _patientWithCancerBreast.CanBeTreatedBy(_advancedTreatmentRoom);
 
+            Assert.True(result);
         }
 
         [Fact]
         public void Advanced_Treatment_Room_can_not_treat_patient_with_flu()
         {
+            var result = _patientWithFlu.CanBeTreatedBy(_advancedTreatmentRoom);
 
+            Assert.False(result);
         }
+
+        [Fact]
+        public void Simple_Treatment_Room_can_not_treat_patient_with_head_cancer()
+        {
+            var result = _patientWithCancerHead.CanBeTreatedBy(_simpleTreatmentRoom);
+
+            Assert.False(result);            
+        }
+
+        [Fact]
+        public void Simple_Treatment_Room_can_not_treat_patient_with_neck_cancer()
+        {
+            var result = _patientWithCancerNeck.CanBeTreatedBy(_simpleTreatmentRoom);
+
+            Assert.False(result);      
+        }
+
+        [Fact]
+        public void Simple_Treatment_Room_can_treat_patient_with_breast_cancer()
+        {
+            var result = _patientWithCancerBreast.CanBeTreatedBy(_simpleTreatmentRoom);
+
+            Assert.True(result);      
+        }
+
+        [Fact]
+        public void Simple_Treatment_Room_can_not_treat_patient_with_flu()
+        {
+            var result = _patientWithFlu.CanBeTreatedBy(_simpleTreatmentRoom);
+
+            Assert.False(result);      
+        }        
+
+        [Fact]
+        public void Normal_Treatment_Room_can_not_treat_patient_with_head_cancer()
+        {
+            var result = _patientWithCancerHead.CanBeTreatedBy(_normalTreatmentRoom);
+
+            Assert.False(result);                  
+        }
+
+        [Fact]
+        public void Normal_Treatment_Room_can_not_treat_patient_with_neck_cancer()
+        {
+            var result = _patientWithCancerNeck.CanBeTreatedBy(_normalTreatmentRoom);
+
+            Assert.False(result);      
+        }
+
+        [Fact]
+        public void Normal_Treatment_Room_can_not_treat_patient_with_breast_cancer()
+        {
+            var result = _patientWithCancerBreast.CanBeTreatedBy(_normalTreatmentRoom);
+
+            Assert.False(result);      
+        }
+
+        [Fact]
+        public void Normal_Treatment_Room_can_treat_patient_with_flu()
+        {
+            var result = _patientWithFlu.CanBeTreatedBy(_normalTreatmentRoom);
+
+            Assert.True(result);      
+        }   
     }
 }
